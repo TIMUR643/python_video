aa522e49e373363f1084db09a4482dd30a83f937
TIMUR643 <82447443+TIMUR643@users.noreply.github.com>
2021-04-26 14:43:32 +0300

    Create 8_2.py

---
+++ 8_2.py
@@ -0,0 +1,9 @@
+def find_max(num1, num2, num3):
+    result = max(a, b, c)
+    return result
+
+a = int(input('Enter number 1: '))
+b = int(input('Enter number 2: '))
+c = int(input('Enter number 3: '))
+
+print(find_max(a, b, c))
